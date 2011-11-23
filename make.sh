ce2606baf535f4006fce859fc8e98d901b0dc6ad
gcastigl <gcastigl@gmail.com>
2011-11-23 16:13:56 -0300

    make.sh (not fnished but it is a start...)

---
+++ make.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+# Cleanup
+rm -rf bin
+mkdir bin
+cd ./bin
+
+# variables
+FLAGS='-Wall -pedantic -std=c99'
+INCLUDE='../src/'
+
+# Compile lex files
+touch chess.c
+
+# Compile and link with other C sources
+gcc $FLAGS -I$INCLUDE *.c ../src/logic/*.c -lfl -o -g
